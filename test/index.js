f1b30d0e098997af7b91a3ebe500cd68b19f2f06
Phaninder Pasupula <pasupulaphani@gmail.com>
2017-02-10 15:11:31 +0000

    dded test db url

--- test/index.js
+++ test/index.js
@@ -4,14 +4,14 @@ const psAnalyticsClient = require("../index");
 describe("psAnalyticsClient", () => {
 
   const options = {
-    pgOptions: "postgres://pearlshare:76DX2bkXJJX9JQJaUe@ps-analytics.cabxqlyyiccn.eu-west-1.rds.amazonaws.com:5432/ps_analytics_pro"
+    pgOptions: "postgres://postgres:badgermushroom@192.168.33.17:5432/ps-api-development"
   };
+  psAnalyticsClient.init(options);
 
   describe("sendSuggestLog", () => {
 
     it("should send suggestLog", () => {
 
-      psAnalyticsClient.init(options);
 
       return psAnalyticsClient.sendSuggestLog({
         url: '/pearls/suggest?q=cafes%20in%20london&my-lat=51.530914599999996&my-lon=-0.1820927&my-address=&_limit=8'
